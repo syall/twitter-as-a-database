c518779167e7e6955c2b873ffde780abf0f997b0
syall <s.yuan.all@gmail.com>
2019-11-30 14:50:33 -0500

    Simple Authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const twitter = require('twitter');
-
+const bodyParser = require('body-parser');
 const { name, version } = require('../package.json');
 require('dotenv').config();
 
@@ -11,6 +11,13 @@ app.listen(PORT, () => {
 	console.log(`${name} running on http://${HOST}:${PORT}`);
 });
 
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(bodyParser.json());
+
 const {
 	CONSUMER_PUBLIC_KEY: consumer_key,
 	CONSUMER_SECRET_KEY: consumer_secret,
@@ -19,7 +26,7 @@ const {
 	DEFAULT_DB,
 } = process.env;
 
-app.get('/db/:user?', async (req, res) => {
+app.get('/users', async (req, res) => {
 	try {
 		const client = new twitter({
 			consumer_key,
@@ -28,35 +35,89 @@ app.get('/db/:user?', async (req, res) => {
 			access_token_secret,
 		});
 		const tweets = await client.get('statuses/user_timeline', {
-			screen_name: req.params.user || DEFAULT_DB,
+			screen_name: DEFAULT_DB,
 		});
-		const users = tweets.map(tweet => {
-			const ret = {};
-			tweet.entities.hashtags.forEach(hashtag => {
-				const [attr] = hashtag.text.match(/[A-Z]+/g);
-				const [val] = hashtag.text.match(/[a-z0-9]+/g);
-				const len = attr.length;
-				if (attr.endsWith('STR'))
-					ret[attr.substring(0, len - 3)] = val;
-				else if (attr.endsWith('BOOL'))
-					ret[attr.substring(0, len - 4)] = val === 'true';
-				else if (attr.endsWith('NUM'))
-					ret[attr.substring(0, len - 3)] = Number.parseInt(val);
-				else
-					ret[attr] = val;
+		const users = tweets
+			.map(t => t.entities.hashtags.map(h => h.text))
+			.map(tags => {
+				const ret = {};
+				tags.forEach(hashtag => {
+					const [attr] = hashtag.match(/[A-Z]+/g);
+					const [val] = hashtag.match(/[a-z0-9]+/g);
+					const len = attr.length;
+					if (attr.endsWith('STR'))
+						ret[attr.substring(0, len - 3).toLowerCase()] = val;
+					else if (attr.endsWith('BOOL'))
+						ret[attr.substring(0, len - 4).toLowerCase()] = val === 'true';
+					else if (attr.endsWith('NUM'))
+						ret[attr.substring(0, len - 3).toLowerCase()] = Number.parseInt(val);
+					else
+						ret[attr.toLowerCase()] = val;
+				});
+				return ret;
 			});
-			return ret;
-		});
-		res.json(users);
+		res.json(users.map(u => {
+			return {
+				user: u.user,
+				active: u.active
+			};
+		}));
 	} catch (err) {
 		res.status(400);
 		res.json(err);
 	}
 });
 
+app.get('/login', async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const client = new twitter({
+			consumer_key,
+			consumer_secret,
+			access_token_key,
+			access_token_secret,
+		});
+		const users = await client.get('statuses/user_timeline', {
+			screen_name: DEFAULT_DB,
+		});
+		const [user] = users
+			.map(t => t.entities.hashtags.map(h => h.text))
+			.filter(u => u.includes(`USERSTR${username}`))
+			.map(tags => {
+				const ret = {};
+				tags.forEach(hashtag => {
+					const [attr] = hashtag.match(/[A-Z]+/g);
+					const [val] = hashtag.match(/[a-z0-9]+/g);
+					const len = attr.length;
+					if (attr.endsWith('STR'))
+						ret[attr.substring(0, len - 3).toLowerCase()] = val;
+					else if (attr.endsWith('BOOL'))
+						ret[attr.substring(0, len - 4).toLowerCase()] = val === 'true';
+					else if (attr.endsWith('NUM'))
+						ret[attr.substring(0, len - 3).toLowerCase()] = Number.parseInt(val);
+					else
+						ret[attr.toLowerCase()] = val;
+				});
+				return ret;
+			});
+		if (!user)
+			throw new Error('User not found.');
+		if (password.localeCompare(user.password))
+			throw new Error('Password incorrect.');
+		res.json({
+			message: 'Authentication Successful!'
+		});
+	} catch (err) {
+		res.status(400).json({
+			message: err.message
+		});
+	}
+});
+
 app.get('*', (req, res) => res.json({
 	service: `${name} ${version}`,
 	routes: [
-		`/db/:user?`,
+		`/users`,
+		`/login`,
 	],
 }));
