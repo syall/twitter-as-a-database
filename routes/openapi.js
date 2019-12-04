f5ab90f2582ccfd53b5461c468b47bad7094d556
syall <s.yuan.all@gmail.com>
2019-12-04 11:30:50 -0500

    Migrate to https and bin/www convention

--- routes/openapi.js
+++ routes/openapi.js
@@ -2,7 +2,7 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const router = express.Router();
 
-const contract = require('../openapi.json');
+const contract = require('../config/openapi.json');
 
 const options = {
 	customSiteTitle: 'Twitter as a Database',
